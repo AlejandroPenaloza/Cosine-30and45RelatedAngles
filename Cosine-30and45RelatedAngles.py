81ef04bcc475e545c17a7f9b446f7f2a9fdc7398
Alejandro Penaloza <alejandro.penaloza.20@gmail.com>
2020-04-20 10:20:16 -0400

    cosine2

--- Cosine-30and45RelatedAngles.py
+++ Cosine-30and45RelatedAngles.py
@@ -1,7 +1,11 @@
-#THIS IS Cosine-30and45RelatedAngles.py, where you can get the value of the cosine of a given angle related from
-#30 degrees - pi/6 and 45 degrees - pi/4
-print("\nCOSINE OF PI/6 (30 degrees) AND PI/4 (45 degrees) RELATED ANGLES\n")
+'''This is Cosine-30and45RelatedAngles.py, where you can get the value of the cosine of a given angle related from
+30 degrees - pi/6 and 45 degrees - pi/4.
+An angle is 30-45 related when it can be gotten by applying half-angle cosine formulae.'''
 
+import math
+
+
+print("\nCOSINE OF PI/6 (30 degrees) AND PI/4 (45 degrees) RELATED ANGLES\n")
 angle = float(input("Input your angle in degrees\n"))
 cosine_inner_signs = []
 var_angle = angle
@@ -13,10 +17,12 @@ elif var_angle == 45.0:
 else:
     while True:
         if var_angle == 30.0:
-            print("Cosine = ", "".join(cosine_inner_signs), 3)
+            print("cos(", angle, " d) = cos(", math.radians(angle), " rad) = ", "sr(2", "sr(2".join(cosine_inner_signs), "sr(3", ")"*(len(cosine_inner_signs) + 1), " /2", sep="")
+            print("where sr means square root")
             break
         elif var_angle == 45.0:
-            print("Cosine = ", "".join(cosine_inner_signs), 2)
+            print("Cosine = ", "sr(2", "sr(2".join(cosine_inner_signs), "sr(2", ")"*(len(cosine_inner_signs) + 1), " /2", sep="")
+            print("where sr means square root")
             break
         elif var_angle != 15.0 and str(var_angle).split(".")[1] == "0":
             print("Angle not related")
